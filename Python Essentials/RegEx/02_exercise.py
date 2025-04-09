6cd986aafb9bfc7fbe31c85effaf81428463e884
Angel Ramirez <angelgabriel.16ra@gmail.com>
2025-04-08 20:19:04 -0600

    RegEx final part

--- Python Essentials/RegEx/02_exercise.py
+++
@@ -1,6 +0,0 @@
-# Ejercicio 02
-# Encuentra todas las ocurrencias de la palabra "midu" en el siguiente texto e indica en que posicion empieza y termina cada coincidencia y uantas veces se encontro.
-
-text= 'Este es el curso de Python de Midudev. ¡Suscribite a midudev si te gusta este contenido! midu'
-
-pattern = 'midu'
--- Python Essentials/RegEx/06_exercise.py
+++
@@ -1,3 +0,0 @@
-# Ejercicio 06:
-#Tenemos una lista de archivos, necesitamos saber los nombre sde los ficheros con extension .txt
-files = 'file1.txt file2.pdf midu-of.webp secret.txt'
