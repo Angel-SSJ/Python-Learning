fcefbb43c0a488790cecc406315d0b3533a22a45
Angel Ramirez <angelgabriel.16ra@gmail.com>
2025-05-05 11:04:05 -0600

    Introductory class -Docstrings

---
+++ Python-Essentials/Documenting-Python-Code/01_example_single-line.py
@@ -0,0 +1,7 @@
+def square(n):
+    '''Take a number n adn return the square of n.'''
+    return n**2
+print(square.__doc__)
+
+# docstring for the built-in function print()
+print(print.__doc__)
---
+++ Python-Essentials/Documenting-Python-Code/02_example_modules.py
@@ -0,0 +1,4 @@
+import pickle
+
+print(pickle.__doc__)
+print(pickle.dumps.__doc__)
